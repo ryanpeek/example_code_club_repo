00df2a73f46115e91e8837e67acb9cfac1a4000b
ryanpeek <rapeek@ucdavis.edu>
2020-11-05 16:50:57 -0800

    make some additions and a deletion

--- scripts/using_github.R
+++ scripts/using_github.R
@@ -8,5 +8,7 @@ use_git_config()
 # get a situation report
 git_sitrep()
 
-# vaccinate helps move files you don't want to be committing/storing publicly to gitignore
 git_vaccinate()
+# git vaccinate does stuff
+
+# add some more notes
