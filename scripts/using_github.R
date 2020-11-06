74c787771036663fd7283a2b907b64fadcc6cd1a
ryanpeek <rapeek@ucdavis.edu>
2020-11-05 16:46:20 -0800

    starting new project

---
+++ scripts/using_github.R
@@ -0,0 +1,12 @@
+# using git
+
+library(usethis)
+
+# set up username
+use_git_config()
+
+# get a situation report
+git_sitrep()
+
+# vaccinate helps move files you don't want to be committing/storing publicly to gitignore
+git_vaccinate()
